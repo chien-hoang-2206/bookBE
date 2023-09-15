0890c53907a7f94b7512ce7556f7cba9e700ce68
chienhv <hoangvanchien2206@gmail.com>
2023-09-15 16:45:04 +0700

    done

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ import express from 'express';
 import homeRouter from './routes/home';
 import aboutRouter from './routes/about';
 
-const app = express();
+export const app = express();
 
 app.use('/home', homeRouter);
 app.use('/about', aboutRouter);
 
-app.listen(4096, () => {
-  console.log('App is running on port 4096');
-});
 ``
